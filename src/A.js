0b5bce4226509d98cc7123063176095bfa979224
johir-rayhan <johir.rayhan01@gmail.com>
2018-03-25 00:35:14 +0600

    initial commit

---
+++ src/A.js
@@ -0,0 +1,28 @@
+// First Example
+// const bar = 'test';
+// const foo = bar + 24;
+
+// Second Example
+// const obj = {
+//   name: 'ray',
+//   age: 24
+// };
+// document.write(foo + obj.name);
+
+
+// 3rd Example
+// const work = new Promise((resolve, reject) => {
+//   resolve('some data');
+// });
+
+// work.then((data) => {
+//   console.log(data);
+// });
+
+// 4th Example
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsx = <p>Hello React</p>;
+
+ReactDOM.render(jsx, document.getElementById('app'));
